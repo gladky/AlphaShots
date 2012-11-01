83d5ac7425424c2208cc2f94b62490053a0593d6
gladky <maciej.gladki@gmail.com>
2012-11-01 14:38:02 -0700

    contact site

--- website-nextVer/kontakt.php
+++ website-nextVer/kontakt.php
@@ -3,6 +3,11 @@
 	<?php include("includes/logo_and_nav.php"); ?>
 	<div class="content"> <!-- Content container -->
 		<div class="widget">
+			<div class = "contact">
+				<h2>Kontakt</h2>
+				<p class="contact">Chetnie odpowiemy na ka¿de Twoje pytanie. Napisz lub zadzwoñ, to nic nie kosztuje.</p>
+				<p class="contact">info@alphashots.pl<br/>123-456-789</p>
+			</div>
 			<div class = "option-wrapper">
 				<p class="branding"><a href="panorama.php">Panorama sferyczna</a></p>
 				<p class="branding"><a href="fotografia.php">Fotografia</a></p>
