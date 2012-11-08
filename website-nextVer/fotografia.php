412fb3e7556ed3b2ebb08066cf4c1310e0652cd9
gladky <maciej.gladki@gmail.com>
2012-11-08 13:58:21 -0800

    Offer - sample product, content style

---
+++ website-nextVer/fotografia.php
@@ -0,0 +1,21 @@
+<?php include("includes/header_and_links.php"); ?>
+<div class="container"> <!--Main container -->
+	<?php include("includes/logo_and_nav.php"); ?>
+	<div class="content"> <!-- Content container -->
+		<div class="widget">
+			<div class = "widget_container">
+				<h2>Fotografia</h2>
+				<p class="widget_text">Chetnie odpowiemy na ka¿de Twoje pytanie. Napisz lub zadzwoñ, to nic nie kosztuje.</p>
+				<p class="widget_text">info@alphashots.pl<br/>123-456-789</p>
+			</div>
+			<div class = "option-wrapper">
+				<p class="branding"><a href="panorama.php">Panorama sferyczna</a></p>
+				<p class="branding"><a href="fotografia.php">Fotografia</a></p>
+				<p class="branding"><a href="index.php">Galeria</a></p>
+				<p class="branding"><a href="oferta.php">Oferta</a></p>
+				<p class="branding"><a href="kontakt.php">Kontakt</a></p>
+			</div>
+		</div>
+	</div><!-- end .content -->
+</div><!-- end .container -->
+<?php include("includes/footer.php"); ?>
--- website-nextVer/oferta.php
+++ website-nextVer/oferta.php
@@ -3,7 +3,7 @@
 	<?php include("includes/logo_and_nav.php"); ?>
 	<div class="content"> <!-- Content container -->
 		<div class="offer" id="js-offer" style="overflow:none;">
-			<h3>Panoramy</h3>
+			<h3>Panoramy Sferyczne</h3>
 			<div class="offer-em">
 				<ul class="accordin">
 					<li class="accordin">
@@ -11,11 +11,12 @@
 							<div class="vertical">
 								<p class="vertical" id="p1">Standard</p>
 							</div>
-							<img class="product" src="pictures/thum1.jpg" />
+							<img class="product" src="images/offer/product-sample.gif" />
 							<p class="accordin">
-								<strong>Standard</strong><br/>
-								Najlepsza cena, standardowa jakoœæ wykonania </br>
+								<span class="offer-name">Standard</span>
+								Najlepsza cena, standardowa jakoœæ wykonania </br></br>
 								Ceny od <span class="blue">99z³</span>
+								<span class="small">sprawdz szczegó³owy cennik</span>
 							</p>
 						</a>
 					</li>
@@ -24,11 +25,12 @@
 							<div class="vertical">
 								<p class="vertical">Premium</p>
 							</div>
-							<img class="product" src="pictures/thum1.jpg" />
+							<img class="product" src="images/offer/product-sample.gif" />
 							<p class="accordin">
-								<strong>Premium</strong><br/>
-								Szczególna dba³oœæ o szczegó³y, najlepsza jakoœæ, przyspieszony czas wykonania </br>
+								<span class="offer-name">Premium</span>
+								Szczególna dba³oœæ o szczegó³y, najlepsza jakoœæ, przyspieszony czas wykonania </br></br>
 								Ceny od <span class="blue">149z³</span>
+								<span class="small">sprawdz szczegó³owy cennik</span>
 							</p>
 						</a>
 					</li>
@@ -40,25 +42,28 @@
 					<li class="accordin2">
 						<a class="accordin2" id="a2">
 							<div class="vertical">
-								<p class="vertical" id="p1">sesyjna</p>
+								<p class="vertical" id="p1">Sesyjna</p>
 							</div>
-							<img class="product" src="pictures/thum1.jpg" />
+							<img class="product" src="images/offer/product-sample.gif" />
 							<p class="accordin">
-								<strong>Freebies3</strong><br/>
-								Download free files to make your job easier.
+								<span class="offer-name">Sesje zdjêciowe</span>
+								Download free files to make your job easier.</br></br>
+								Ceny od <span class="blue">199z³</span>
+								<span class="small">sprawdz szczegó³owy cennik</span>
 							</p>
 						</a>
 					</li>
 					<li class="accordin2">
 						<a class="accordin2" id="b2">
 							<div class="vertical">
-								<p class="vertical">reklamowa</p>
+								<p class="vertical">Reklamowa</p>
 							</div>
-							<img class="product" src="pictures/thum1.jpg" />
+							<img class="product" src="images/offer/product-sample.gif" />
 							<p class="accordin">
-								<strong>Sesje reklamowe</strong><br/>
-								Profesjonalne zdjêcia produktu, aran¿acja sceny, interakcja z zamawiaj¹cym</br>
+								<span class="offer-name">Sesje reklamowe</span>
+								Profesjonalne zdjêcia produktu, aran¿acja sceny, interakcja z zamawiaj¹cym</br></br>
 								Ceny od <span class="blue">199z³</span>
+								<span class="small">sprawdz szczegó³owy cennik</span>
 							</p>
 						</a>
 					</li>
